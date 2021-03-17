51795b81786082df9f186acc0b7e9775d3567e56
finality5 <tanagon601@gmail.com>
2021-03-17 22:04:17 +0700

    add preview image

--- src/Component/ReviewIndex.js
+++ src/Component/ReviewIndex.js
@@ -16,8 +16,32 @@ import AddQuizModal from "./AddQuizModal";
 import axios from "axios";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
 
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import StarBorder from "@material-ui/icons/StarBorder";
+import PersonIcon from "@material-ui/icons/Person";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import Tooltip from "@material-ui/core/Tooltip";
+import WarningIcon from '@material-ui/icons/Warning';
+import AssignmentLateIcon from '@material-ui/icons/AssignmentLate';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 const styles = (theme) => ({
+  root: {
+    width: "100%",
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
   paper: {
     maxWidth: 936,
     margin: "auto",
@@ -68,6 +92,16 @@ const quizType = (type) => {
   }
 };
 
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    color: "rgba(0, 0, 0, 0.87)",
+    minWidth: 415,
+    
+    boxShadow: theme.shadows[1]
+  },
+}))(Tooltip);
+
 const theme = createMuiTheme({
   overrides: {
     // Style sheet name ⚛️
@@ -93,20 +127,45 @@ const ReviewIndex = (props) => {
     setMode,
     setCurrentQuiz,
   } = useContext(AppContext);
-  const [score, setScore] = useState();
+  const [data, setData] = useState();
+
+  const [grade, setGrade] = useState([]);
+  const [ungrade, setUngrade] = useState([]);
+  const [duplicate, setDuplicate] = useState([]);
+  const [open, setOpen] = useState([]);
+
   useEffect(() => {
     if (currentQuiz) {
       const url = `http://${host}:5000/getscore?uid=${user.uid}&class_key=${currentQuiz.classKey}&quiz_key=${currentQuiz.quizKey}`;
       axios
         .get(url)
         .then((res) => {
-          setScore(res.data.score_data);
+          setData(res.data.score_data);
         })
         .catch((err) => console.log(err.message));
     }
   }, [user, host, currentQuiz]);
-  console.log(score);
+  //console.log(grade, ungrade, duplicate);
 
+  useEffect(() => {
+    if (data) {
+      setOpen([...data.map((obj) => ({ key: obj.student_key, open: false }))]);
+      setGrade([...data.filter((obj) => obj.graded && !obj.duplicate)]);
+      setUngrade([...data.filter((obj) => !obj.graded && !obj.duplicate)]);
+      setDuplicate([...data.filter((obj) => obj.graded && obj.duplicate)]);
+    }
+  }, [data]);
+  //console.log(gradeOpen)
+
+  const handleOpen = (id) => {
+    //console.log(id);
+    setOpen([
+      ...open.map((obj) =>
+        obj.key === id ? { ...obj, open: !obj.open } : obj
+      ),
+    ]);
+  };
+  //console.log(open);
   return (
     <Paper className={classes.paper}>
       <AppBar
@@ -116,7 +175,7 @@ const ReviewIndex = (props) => {
         elevation={0}
       >
         <Toolbar>
-          {currentQuiz ? (
+          {data ? (
             <Grid container spacing={2} alignItems="center">
               <Grid item xs>
                 <ThemeProvider theme={theme}>
@@ -148,7 +207,161 @@ const ReviewIndex = (props) => {
           ) : null}
         </Toolbar>
       </AppBar>
-      <div className={classes.contentWrapper}>TEST</div>
+      <div className={classes.contentWrapper}>
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Graded
+            </ListSubheader>
+          }
+          className={classes.root}
+        >
+          {grade
+            ? grade.map((obj) => (
+                <React.Fragment key={obj.student_key}>
+                  <ListItem button onClick={() => handleOpen(obj.student_key)}>
+                    <ListItemIcon>
+                      <AccountBoxIcon style={{ fill: "green" }} />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={`${obj.student_id} | ${obj.student_name}`}
+                    />
+                    {open.find((item) => item.key === obj.student_key).open ? (
+                      <ExpandLess />
+                    ) : (
+                      <ExpandMore />
+                    )}
+                  </ListItem>
+                  <Collapse
+                    in={open.find((item) => item.key === obj.student_key).open}
+                    timeout="auto"
+                    unmountOnExit
+                  >
+                    <List component="div" disablePadding>
+                      {obj.quiz
+                        ? obj.quiz.map((obj2) => (
+                            <ListItem
+                              button
+                              className={classes.nested}
+                              key={obj2.quiz_key}
+                            >
+                              <Grid
+                                container
+                                direction="row"
+                                justify="flex-start"
+                                alignItems="center"
+                              >
+                              <HtmlTooltip
+                                placement="right"
+                                  title={
+                                    <React.Fragment>
+                                      <img width={400} src={obj2.url} alt="score" />
+                                    </React.Fragment>
+                                  }
+                                >
+                                  <img width={100} src={obj2.url} alt="score" />
+                                </HtmlTooltip>
+                              </Grid>
+                            </ListItem>
+                          ))
+                        : null}
+                    </List>
+                  </Collapse>
+                </React.Fragment>
+              ))
+            : null}
+        </List>
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Ungraded
+            </ListSubheader>
+          }
+          className={classes.root}
+        >
+          {ungrade
+            ? ungrade.map((obj) => (
+                <React.Fragment key={obj.student_key}>
+                  <ListItem button onClick={() => handleOpen(obj.student_key)}>
+                    <ListItemIcon>
+                      <AccountBoxIcon style={{ fill: "grey" }} />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={`${obj.student_id} | ${obj.student_name}`}
+                    />
+                    {open.find((item) => item.key === obj.student_key).open ? (
+                      <ExpandLess />
+                    ) : (
+                      <ExpandMore />
+                    )}
+                  </ListItem>
+                  <Collapse
+                    in={open.find((item) => item.key === obj.student_key).open}
+                    timeout="auto"
+                    unmountOnExit
+                  >
+                    <List component="div" disablePadding>
+                      <ListItem button className={classes.nested}>
+                        <ListItemIcon>
+                          <AssignmentLateIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Not graded yet" />
+                      </ListItem>
+                    </List>
+                  </Collapse>
+                </React.Fragment>
+              ))
+            : null}
+        </List>
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Duplicated
+            </ListSubheader>
+          }
+          className={classes.root}
+        >
+          {duplicate
+            ? duplicate.map((obj) => (
+                <React.Fragment key={obj.student_key}>
+                  <ListItem button onClick={() => handleOpen(obj.student_key)}>
+                    <ListItemIcon>
+                      <WarningIcon style={{ fill: "orange" }} />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={`${obj.student_id} | ${obj.student_name}`}
+                    />
+                    {open.find((item) => item.key === obj.student_key).open ? (
+                      <ExpandLess />
+                    ) : (
+                      <ExpandMore />
+                    )}
+                  </ListItem>
+                  <Collapse
+                    in={open.find((item) => item.key === obj.student_key).open}
+                    timeout="auto"
+                    unmountOnExit
+                  >
+                    <List component="div" disablePadding>
+                      <ListItem button className={classes.nested}>
+                        <ListItemIcon>
+                          <StarBorder />
+                        </ListItemIcon>
+                        <ListItemText primary="Starred" />
+                      </ListItem>
+                    </List>
+                  </Collapse>
+                </React.Fragment>
+              ))
+            : null}
+        </List>
+      </div>
     </Paper>
   );
 };
